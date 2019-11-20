9b9b9b519263d9508cb4875956b34c0fd51adb8a
AdamMomen <adammuman81@gmail.com>
2019-11-20 09:50:34 +0100

    Self Assessment

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -13,44 +13,61 @@
 //
 // EXAMPLE:
 //
-// var familyTree = {
-//   'firstName': 'Beth',
-//   'lastName': 'Johnson',
-//   'location': 'San Francisco',
-//   'children': [
-//     {
-//       'firstName': 'Beth Jr.',
-//       'lastName': 'Johnson',
-//       'location': 'Berkeley',
-//       'children': [
-//         {
-//           'firstName': 'Smitty',
-//           'lastName': 'Won',
-//           'location': 'Beijing',
-//           'children': []
-//         }
-//       ]
-//     },
-//     {
-//       'firstName': 'Joshie',
-//       'lastName': 'Wyattson',
-//       'location': 'Berkeley',
-//       'children': []
-//     }
-//   ]
-// };
-//
-// var livesInBerkeley = function (familyMember) {
-//   return familyMember.location === 'Berkeley';
-// }
-//
-// filterFamilyMembers(familyTree, livesInBerkeley)
-//
-// returns ['Beth Jr. Johnson', 'Joshie Wyattson'];
+var familyTree = {
+  'firstName': 'Beth',
+  'lastName': 'Johnson',
+  'location': 'San Francisco',
+  'children': [
+    {
+      'firstName': 'Beth Jr.',
+      'lastName': 'Johnson',
+      'location': 'Berkeley',
+      'children': [
+        {
+          'firstName': 'Smitty',
+          'lastName': 'Won',
+          'location': 'Beijing',
+          'children': []
+        }
+      ]
+    },
+    {
+      'firstName': 'Joshie',
+      'lastName': 'Wyattson',
+      'location': 'Berkeley',
+      'children': []
+    }
+  ]
+};
+// pseodo Code
+// Base Case or condiotion for stoping the recusion is when the index reaches the length array of children of the origin family
+//2. checking if we have sub families in our tries , if there is send  assign the family name in the cheldren array as a familyTree and recursiveley calling the function 
+//3. each recurive it checks for the sub family trees with iteration of the index
+//4. using the truth test to check the location of the subfamily is  Berkeley;
+//if true push the first and last name of the family to filterdFamiles array.
+// function recursivley call itself untill the iterator reaches threr's the length of cheldren array 
+var filterFamilyMembers = function (familyTree, truthTest, filterdFamiles, index) {
 
+var index = index || 0;
+var filterdFamilesArr = filterdFamilesArr || [];
 
-
-var filterFamilyMembers = function (familyTree, truthTest) {
-  // All your code in this function body
+	//Recursion Terminitaion condition 
+	if (index === familyTree.children.length - 1) {
+		return filterdFamiles;
+	}
+	if(familyTree.children[index].length > 0) {
+		return  filterFamilyMembers(familyTree.children[index], truthTest, filterdFamiles, index);
+	} else {
+		index++;
+	}
+  if (truthTest(familyTree.children[index])) {
+  	filterdFamilesArr.push(familyTree.children[index].firstName +' ' + familyTree.children[index].lastName);
+  }
+  return filterFamilyMembers (familyTree, truthTest, filterdFamiles, index);
 };
 
+var livesInBerkeley = function (familyMember) {
+  return familyMember.location === 'Berkeley';
+}
+ filterFamilyMembers(familyTree, livesInBerkeley);
+ // returns ['Beth Jr. Johnson', 'Joshie Wyattson'];
